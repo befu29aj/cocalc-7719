deec5168ec8908ffdf432b2d2ecddbfff7c03657
Stefan Lukits <sediomyle@gmail.com>
2017-11-28 10:28:59 -0800

    i

--- auto/ft-12-Optimization.el
+++ auto/ft-12-Optimization.el
@@ -3,6 +3,15 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "xcolor=dvipsnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
@@ -31,12 +40,22 @@
     "eq:aweefahx"
     "eq:ebukatio"
     "eq:inaidimo"
+    "eq:peimoojo"
+    "eq:udaiguph"
+    "eq:tohwoiph"
+    "eq:ahyoimij"
+    "eq:uzuwooba"
+    "eq:iboohoht"
+    "eq:waduoqui"
     "eq:ohkaedoy"
     "eq:teecakie"
     "eq:saepaego"
     "eq:xohsaemu")
    (LaTeX-add-counters
     "expls"
-    "exercise"))
+    "exercise")
+   (LaTeX-add-xcolor-definecolors
+    "red"
+    "myblue"))
  :latex)
 
