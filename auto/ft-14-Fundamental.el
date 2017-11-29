ea6f3083d7e1ff71f644fb8038c3d39268d98d15
Stefan Lukits <sediomyle@gmail.com>
2017-11-29 08:28:36 -0800

    i

---
+++ auto/ft-14-Fundamental.el
@@ -0,0 +1,75 @@
+(TeX-add-style-hook
+ "ft-14-Fundamental"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor=dvipsnames")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "cancel"
+    "graphicx"
+    "wrapfig"
+    "colortbl"
+    "color"
+    "alltt")
+   (TeX-add-symbols
+    '("ubung" 0)
+    '("beispiel" 1)
+    "CourseName"
+    "CourseNumber"
+    "CourseInst"
+    "qeq"
+    "QEQ")
+   (LaTeX-add-labels
+    "eq:aiceiphe"
+    "eq:pheiwaot"
+    "eq:ahngohto"
+    "eq:ogheigha"
+    "eq:joogeipo"
+    "eq:xoozazui"
+    "eq:eiyahcei"
+    "eq:muixutoh"
+    "eq:eighooko"
+    "eq:ungiumah"
+    "eq:eevoomoh"
+    "eq:eohauthu"
+    "eq:heizieta"
+    "eq:zohjieph"
+    "eq:oodaelad"
+    "eq:eeshooyo"
+    "eq:uafievai"
+    "eq:igheivei"
+    "eq:akahheju"
+    "eq:zeifahce"
+    "eq:afahthud"
+    "eq:desheiga"
+    "eq:oongaiqu"
+    "eq:ahxiequo"
+    "eq:beavaika"
+    "eq:paeshohz"
+    "eq:phiasohm"
+    "eq:yooxiefi"
+    "eq:ufaetohm"
+    "eq:vahyooyi"
+    "eq:wohbuxoo"
+    "eq:ohgooquu"
+    "eq:yoocaitu"
+    "eq:zahrahsi"
+    "eq:eikaidei"
+    "eq:pukaepha"
+    "eq:bicochoh"
+    "eq:chabahxo"
+    "eq:ovooleek"
+    "eq:vichujai"
+    "eq:veigheph"
+    "eq:iofaepha"
+    "eq:booghuey"
+    "eq:xusicahk"
+    "eq:quaechea"
+    "eq:iigahdae")
+   (LaTeX-add-counters
+    "expls"
+    "exercise"))
+ :latex)
+
