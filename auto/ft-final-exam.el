72f1f9d760b8373846233dfad78d481ffb574079
Stefan Lukits <sediomyle@gmail.com>
2017-12-08 10:33:42 -0800

    ft-final-exam.tex

---
+++ auto/ft-final-exam.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "ft-final-exam"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "october")
+   (TeX-add-symbols
+    '("frage" 0))
+   (LaTeX-add-labels
+    "eq:vuomebic"
+    "eq:vaiphook"
+    "eq:ohzaingu"
+    "eq:aoshiehi"
+    "eq:aehoosut")
+   (LaTeX-add-counters
+    "frage"))
+ :latex)
+
