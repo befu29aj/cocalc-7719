cef4fd1033f08682edb468218ec0273fe89247ad
Stefan Lukits <sediomyle@gmail.com>
2018-10-03 18:02:25 -0700

    ft-termtest-A-instructions.tex

---
+++ auto/ft-termtest-A-instructions.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "ft-termtest-A-instructions"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "table")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "enumerate"
+    "syllogism"
+    "october"
+    "xcolor")
+   (TeX-add-symbols
+    '("aufgabe" 0))
+   (LaTeX-add-counters
+    "aufg"))
+ :latex)
+
