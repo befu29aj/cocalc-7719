1f6a9222d831a1515d7d2b613c7409501d6879c5
Stefan Lukits <sediomyle@gmail.com>
2018-10-15 09:24:09 -0700

    ft-08-GrowthDecay.tex

--- auto/ft-08-GrowthDecay.el
+++ auto/ft-08-GrowthDecay.el
@@ -3,33 +3,20 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "xcolor=dvipsnames")))
-   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
     "beamer10"
-    "cancel"
-    "graphicx"
-    "wrapfig"
-    "colortbl"
-    "color"
-    "alltt")
-   (TeX-add-symbols
-    '("ubung" 0)
-    '("beispiel" 1)
-    "CourseName"
-    "CourseNumber"
-    "CourseInst"
-    "qeq"
-    "QEQ")
+    "teachbeamer")
    (LaTeX-add-labels
     "eq:sayeeziu"
     "eq:anuvuama"
@@ -42,12 +29,6 @@
     "eq:peongeex"
     "eq:oopohtee"
     "eq:thaixazu"
-    "eq:ahgheeha")
-   (LaTeX-add-counters
-    "expls"
-    "exercise")
-   (LaTeX-add-xcolor-definecolors
-    "red"
-    "myblue"))
+    "eq:ahgheeha"))
  :latex)
 
